91c4bde64526ff7463acb238f32b9d2ac84ccd63
Gaurav Kumar <gauravkkrchaudhary@gmail.com>
2025-05-14 23:10:13 +0530

    changed png

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,8 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import Portfolio from "../../Assets/Projects/Portfolio.png";
 import chatify from "../../Assets/Projects/chatify.png";
+import Calculator from "../../Assets/Projects/Calculator.png";
+
 
 
 function Projects() {
@@ -20,7 +22,7 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={Calculator}
               isBlog={false}
               title="CalculatorApp"
               description="CalculatorApp is an Android AppliI developed a Basic Calculator App that performs essential arithmetic operations such as addition, subtraction, multiplication, and division. The app features a clean and user-friendly interface, allowing users to input numbers and view results with ease. Built using Java and XML for Android , it handles input validation and displays real-time results, making everyday calculations quick and efficient. This project helped me strengthen my foundational programming skills and understand UI/UX design principlescation using Android Studio. It is written using Java and Xml . User can able to do basic Calculation using this apps. It will be helpful to user who needs help in Calculation."
